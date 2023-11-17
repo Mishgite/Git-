076a0ad31883b819333c533d853cd982443cde6f
75942 <7594201@gmail.com>
2023-11-17 17:14:03 +0300

    1

--- main.py
+++ main.py
@@ -1,37 +1,56 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtCore import Qt
 import sys
-import random
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTableWidget, QTableWidgetItem
+import sqlite3
 
-
-class CircleButton(QMainWindow):
+class CoffeeInfoApp(QWidget):
     def __init__(self):
         super().__init__()
-        self.setGeometry(500, 500, 500, 500)
+        self.setWindowTitle('Coffee Info')
+        self.setGeometry(100, 100, 600, 500)
+        self.layout = QVBoxLayout()
+        self.table_widget = QTableWidget()
+        self.layout.addWidget(self.table_widget)
+        self.setLayout(self.layout)
 
-        self.button = QPushButton("Рисовать", self)
-        self.button.setGeometry(0, 0, 50, 20)
-        self.button.clicked.connect(self.draw_circle)
+        # Подключение к базе данных
+        self.connection = sqlite3.connect('coffee.sqlite')
+        self.cursor = self.connection.cursor()
 
-    def draw_circle(self):
-        self.update()
+        # Получение данных из базы данных
+        self.get_coffee_info()
 
-    def paintEvent(self, event):
-        qp = QPainter()
-        qp.begin(self)
-        self.draw(qp)
-        qp.end()
+    def get_coffee_info(self):
+        # Выполнение SQL-запроса для получения данных
+        self.cursor.execute('SELECT * FROM coffe')
+        coffee_data = self.cursor.fetchall()
 
-    def draw(self, qp):
-        size = random.randint(1, 500)
-        color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        qp.setBrush(color)
-        qp.drawEllipse(random.randint(1, 500), random.randint(1, 500), size, size)
+        # Установка количества строк и столбцов таблицы
+        self.table_widget.setRowCount(len(coffee_data))
+        self.table_widget.setColumnCount(7)
 
+        # Задание заголовков таблицы
+        self.table_widget.setHorizontalHeaderLabels(['ID', 'Название сорта', 'Степень обжарки', 'Молотый/в зернах',
+                                                      'Описание вкуса', 'Цена', 'Объем упаковки'])
+
+        row = 0
+        # Заполнение таблицы данными
+        for data in coffee_data:
+            self.table_widget.setItem(row, 0, QTableWidgetItem(str(data[0])))
+            self.table_widget.setItem(row, 1, QTableWidgetItem(data[1]))
+            self.table_widget.setItem(row, 2, QTableWidgetItem(data[2]))
+            self.table_widget.setItem(row, 3, QTableWidgetItem(data[3]))
+            self.table_widget.setItem(row, 4, QTableWidgetItem(data[4]))
+            self.table_widget.setItem(row, 5, QTableWidgetItem(str(data[5])))
+            self.table_widget.setItem(row, 6, QTableWidgetItem(str(data[6])))
+
+            row += 1
+
+    def closeEvent(self, event):
+        # Закрытие соединения с базой данных при закрытии приложения
+        self.connection.close()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = CircleButton()
+    window = CoffeeInfoApp()
     window.show()
     sys.exit(app.exec_())
